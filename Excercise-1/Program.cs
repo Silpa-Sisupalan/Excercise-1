aef8f10a60ae1b88a888b6554bdbe7fcb13b731b
Silpa.Sisupalan <Silpa.Sisupalan@ust.com>
2021-11-14 12:41:55 +0530

    Add project files.

---
+++ Excercise-1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_1
+{
+    class Program
+    {
+        public void perimeter()//p=2(l+b)
+        {
+            Console.WriteLine("Enter length and breadth of the rectangle:");
+            int l= Convert.ToInt32(Console.ReadLine()); 
+            int b = Convert.ToInt32(Console.ReadLine());
+            double p = 2 * (l + b);
+            Console.WriteLine("Expected Output:"+p); 
+
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.perimeter();
+            Console.ReadLine();
+        }
+    }
+}
